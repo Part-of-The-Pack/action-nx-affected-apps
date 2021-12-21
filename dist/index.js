34591db16ffec921a6afad1687f40104d298061c
Jonathan Dierckens <jonathan.dierckens@inthepocket.mobi>
2021-12-21 08:16:23 +0100

    add NX_AFFECTED_APPS_WITH_IDENTIFIER

--- dist/index.js
+++ dist/index.js
@@ -553,6 +553,7 @@ function run(workspace = '.') {
             });
             core.setOutput('affected_apps', apps);
             core.exportVariable('NX_AFFECTED_APPS', apps);
+            core.exportVariable('NX_AFFECTED_APPS_WITH_IDENTIFIER', JSON.stringify(apps.map(app => `_${app}_`)));
             core.info(`Found these affected apps: \n ${apps}`);
         }
         catch (error) {
--- src/main.ts
+++ src/main.ts
@@ -22,7 +22,10 @@ export async function run(workspace: string = '.'): Promise<void> {
 
         core.setOutput('affected_apps', apps)
         core.exportVariable('NX_AFFECTED_APPS', apps)
-
+        core.exportVariable(
+            'NX_AFFECTED_APPS_WITH_IDENTIFIER',
+            JSON.stringify(apps.map(app => `_${app}_`))
+        )
         core.info(`Found these affected apps: \n ${apps}`)
     } catch (error) {
         core.setFailed((error as Error).message)
